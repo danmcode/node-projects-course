467871a5cf96950b73cbb723b01727a3c413d2be
Daniel Alexander Muelas Rivera <desarrollador@seguridadomega.com.co>
2021-11-26 08:08:46 -0500

    Paquetes de instalacion y package.json

--- 03-Bases-Node/app.js
+++ 03-Bases-Node/app.js
@@ -3,8 +3,10 @@ const { crearArchivo } = require('./helpers/multiplicar');
 console.clear(); //Limpiar consola
 //obtener la base entrada de consola concepto
 
+const [, , arg3 = 'base=5' ] = process.argv;
+const [, base = 5] = arg3.split('=');
 
-const base = 3;
+//const base = 3;
 
 crearArchivo(base)
     .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
