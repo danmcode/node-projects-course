4f1f7bd8f610d9b6c27c36d3ce7143b94b2d322e
Daniel Alexander Muelas Rivera <danielmuelasrivera483@gmail.com>
2022-07-05 07:41:12 -0500

    Usando hbs para servir la informacion * Creando "Parciales para reutilizar codigo HTML"

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,17 +1,27 @@
 const express = require('express');
+//Registrar parciales
+const hbs = require('hbs');
+
 const app = express();
 const port = 8080;
 
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+
 // Servir contenido estatico
-app.use( express.static('public') ); 
+app.use( express.static('public')); 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('home', {
+    nombre: 'Daniel Alexander Muelas',
+    titulo: 'Curso de Node'
+  });
 });
 
-app.get('/home', (req, res) => {
-    res.send('home');
-});
+// app.get('/home', (req, res) => {
+//     res.send('home');
+// });
 
 app.get('/generic', (req, res) => {
     res.sendFile(__dirname + '/public/generic.html');
@@ -21,12 +31,12 @@ app.get('/elements', (req, res) => {
     res.sendFile(__dirname + '/public/elements.html');
 });
 
-app.get('*', (req, res) => {
-    //res.send('404 | Page No Found'); //only send plain text to client
+// app.get('*', (req, res) => {
+//     //res.send('404 | Page No Found'); //only send plain text to client
 
-    // send a dirname to client
-    res.sendFile(__dirname + '/public/404.html');
-});
+//     // send a dirname to client
+//     res.sendFile(__dirname + '/public/404.html');
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
